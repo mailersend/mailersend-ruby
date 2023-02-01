7c9b3ff3b3947b3f2a3b4147a541aa11769c9430
Ahmad Younes <ahmayou@gmail.com>
2023-02-01 17:39:40 +0300

    Removed dotenv dependency

--- lib/mailersend/client.rb
+++ lib/mailersend/client.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 require 'http'
-require 'dotenv/load'
 
 API_URL = 'https://api.mailersend.com/v1'
 API_BASE_HOST = 'api.mailersend.com'
-
-Dotenv.require_keys('MAILERSEND_API_TOKEN')
 
 # mailersend-ruby is a gem that integrates all endpoints from MailerSend API
 module Mailersend
@@ -14,7 +11,7 @@ module Mailersend
 
   # Inits the client.
   class Client
-    def initialize(api_token = ENV['MAILERSEND_API_TOKEN'])
+    def initialize(api_token:)
       @api_token = api_token
     end
 
--- mailersend-ruby.gemspec
+++ mailersend-ruby.gemspec
@@ -30,7 +30,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 2.4.1'
   spec.add_development_dependency 'rake', '~> 13.0'
   spec.add_development_dependency 'rubocop', '~> 1.7'
-  spec.add_dependency 'dotenv', '~> 2.7'
   spec.add_dependency 'http', '~> 5.0'
   spec.add_dependency 'json', '~> 2.5'
   spec.add_dependency 'uri', '~> 0.12.0'
